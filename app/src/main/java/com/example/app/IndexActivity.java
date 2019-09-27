62fc964837b7e1719d58517776a6f39dbe4a3bc5
CletwinE <ecletwin@gmail.com>
2019-09-27 15:05:55 +0530

    Drawer added to index

--- app/src/main/java/com/example/app/IndexActivity.java
+++ app/src/main/java/com/example/app/IndexActivity.java
@@ -1,21 +1,45 @@
 package com.example.app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class IndexActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class IndexActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ImageView adventure, stay, restaurant;
+
+    //Drawer
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        //Drawer
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navagation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
         adventure = findViewById(R.id.adventureIv);
@@ -30,22 +54,74 @@ public class IndexActivity extends AppCompatActivity {
                 startActivity(new Intent(IndexActivity.this, RegisterActivity.class));
             }
         });
-
         stay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(IndexActivity.this, RegisterActivity.class));
             }
         });
-
         restaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(IndexActivity.this, RegisterActivity.class));
             }
         });
-        
+
     }
 
 
+    //Drawer
+    private void checkUserStatus(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null){
+            //mProfileTv.setText(user.getEmail());
+        }
+        else{
+            startActivity(new Intent(IndexActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_addv){
+            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.nav_stays){
+            startActivity(new Intent(IndexActivity.this, ProfileActivity.class));
+        }
+        if(id == R.id.nav_restaurant){
+            startActivity(new Intent(IndexActivity.this, ProfileActivity.class));
+        }
+        if(id == R.id.nav_profile){
+            startActivity(new Intent(IndexActivity.this, ProfileActivity.class));
+        }
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+            finish();
+            return true;
+        }
+        return false;
+    }
+
 }
+
+
